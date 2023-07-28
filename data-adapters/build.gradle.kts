c892904c08b999e0b9702ae99d5e263bddf99055
valb3r <valb3r.ua@gmail.com>
2023-07-28 22:30:19 +0300

    Update version

--- data-adapters/build.gradle.kts
+++ data-adapters/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.valb3r.jsimpleplots"
-version = "1.0-SNAPSHOT"
+version = "0.0.2-SNAPSHOT"
 
 repositories {
     mavenCentral()
--- plots/build.gradle.kts
+++ plots/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.valb3r.jsimpleplots"
-version = "1.0-SNAPSHOT"
+version = "0.0.2-SNAPSHOT"
 
 repositories {
     mavenCentral()
