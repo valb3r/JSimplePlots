bd4f9fa4110abd1298d77833f8616183529e3f70
valb3r <valb3r.ua@gmail.com>
2023-08-01 10:28:56 +0300

    Added forward diff helper

--- plots/src/main/kotlin/com/valb3r/jsimpleplots/plots/helpers/Helpers.kt
+++ plots/src/main/kotlin/com/valb3r/jsimpleplots/plots/helpers/Helpers.kt
@@ -25,3 +25,19 @@ fun DoubleArray.relativeToFirstElem(): DoubleArray {
         it - this[0]
     }.toDoubleArray()
 }
+
+fun FloatArray.diffFwdFirstOrder(dt: Float = 1.0f): FloatArray {
+    val result = FloatArray(this.size)
+    (0..< this.size - 1).forEach {
+        result[it] = (this[it + 1] - this[it]) / dt
+    }
+    return result
+}
+
+fun DoubleArray.diffFwdFirstOrder(dt: Double = 1.0): DoubleArray {
+    val result = DoubleArray(this.size)
+    (0..< this.size - 1).forEach {
+        result[it] = (this[it + 1] - this[it]) / dt
+    }
+    return result
+}
